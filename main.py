04a11dbad80b0a21a82c8f82c94f8a96eff268a6
Martin <mruddym@gmail.com>
2025-03-28 17:20:12 +0200

    Implemented full main() processing of a query

--- main.py
+++ main.py
@@ -3,47 +3,59 @@ from langchain_mistralai import ChatMistralAI
 import yaml
 import os
 from dotenv import load_dotenv
-from sentence_transformers import SentenceTransformer
 
-from create_db import EmbeddingDB
+from manage import EmbeddingDB, prepare_prompt, ReAct_process
 from conversation import Conversation
 from tools.llm_functions import *
 
-# API key setups
-load_dotenv()
-MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-
-# LLm Chat setups
-default_chat = ChatMistralAI(
-    model="open-mistral-7b",
-    api_key=MISTRAL_API_KEY
-)
-
-cheap_chat = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash-8b",
-    google_api_key=GEMINI_API_KEY
-)
-
-embeddings_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-
-# Prompt setups
-with open("prompts.yaml", "r") as file:
-    prompts = yaml.safe_load(file)
-
 def main():
+    # API keys
+    load_dotenv()
+    MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+    # Prepare LLM models
+    default_chat = ChatMistralAI(
+        model="open-mistral-7b",
+        api_key=MISTRAL_API_KEY
+    )
+    cheap_chat = ChatGoogleGenerativeAI(
+        model="gemini-1.5-flash-8b",
+        google_api_key=GEMINI_API_KEY
+    )
+    #embeddings_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
+    # Load system prompts
+    with open("prompts.yaml", "r") as file:
+        prompts = yaml.safe_load(file)
+
+    # Load DB and start processing
     db = EmbeddingDB(db_path="db/user_data.db", faiss_path="db/faiss_index.index")
     conversation = Conversation(default_chat, cheap_chat, prompts)
+    debug = False
     while True:
-        user_input = input("Type question: ")
-        if len(user_input) == 4 and user_input.lower() == "exit":
+        query = input("Type question: ")
+        if len(query) == 4 and query.lower() == "exit":
             break
         # Procesar la query con ReAct framework, dando acceso a las herramientas disponibles
         # Al procesar, introudcir info relevante del usuario y de conversaciones pasadas si procede
+        relevant_user_info = db.search(query, source="user_info")
+        relevant_past_conversations = db.search(query, source="conversation")
+        prompt = prepare_prompt(relevant_user_info, relevant_past_conversations, query)
+        if debug:
+            steps, answer = ReAct_process(llm=default_chat, query=prompt, prompts=prompts, debug=True)
+            print(steps)
+        else:
+            answer = ReAct_process(llm=default_chat, query=prompt, prompts=prompts)
         # Acabar de procesar mediante una query llm para personalizarlo al usuario.
-    final_summary, user_info = conversation.exit_conversation()
-    db.add_conversation_summary(summary_text=final_summary)
-    # Comprobar que la user info sea realmente nueva, para ello hay que anadir una funcion de llm que devuelva solo los statements que osn realmente nuevos
+        final_answer = personalize_final_answer(answer, conversation.conversation_history, default_chat, prompts)
+        print(final_answer)
+        conversation.add_interaction(query, final_answer)
+
+    if len(conversation.conversation_history) != 0:
+        final_summary, user_info = conversation.exit_conversation()
+        db.add_conversation_summary(summary_text=final_summary)
+        # Comprobar que la user info sea realmente nueva, para ello hay que anadir una funcion de llm que devuelva solo los statements que osn realmente nuevos
     db.close_db()
     
 
